05ca3760a4f6e703f3fc0f8813650719437a6440
shubhammani41 <shubhammani41@gmail.com>
2023-08-19 23:00:07 +0530

    Dynamic TextFieldReactive props

--- src/Modules/Global/TextFieldReactive/TextFieldReactive.js
+++ src/Modules/Global/TextFieldReactive/TextFieldReactive.js
@@ -1,24 +1,22 @@
 import { InputLabel, TextField } from "@mui/material"
 import { Validators } from "../ReactiveForms/Validators"
 
-function TextFieldReactive({ onChange, OnFocus, ...props }) {
-    const defaultOnChange = onChange?onChange:()=>null
-    const defaultOnFocus = OnFocus?OnFocus:()=>null
+function TextFieldReactive({ onChange, OnFocus, formControl, label, ...props }) {
+    const defaultOnChange = onChange ? onChange : () => null
+    const defaultOnFocus = OnFocus ? OnFocus : () => null
     return (
         <div className={'textFieldReactive' + (props?.id ? ' ' + props.id : '')}>
-            <InputLabel className={(props?.formControl?.validators?.length > 0 && props.formControl.validators.indexOf(Validators.Required) > -1) ? 'afterRequired' : ''} htmlFor={props?.id ? props.id : ''}>{props?.label ? props.label : ''}</InputLabel>
-            <TextField size={props?.size ? props.size : 'small'} className={props?.className ? props.className : ''} id={props?.id ? props.id : ''}
-                variant={props?.variant ? props.variant : "outlined"} placeholder={props?.placeholder ? props.placeholder : ''}
-                value={props?.formControl?.value ? props.formControl.value : ''}
+            <InputLabel className={(formControl?.validators?.length > 0 && formControl.validators.indexOf(Validators.Required) > -1) ? 'afterRequired' : ''} htmlFor={props?.id ? props.id : ''}>{label ? label : ''}</InputLabel>
+            <TextField value={formControl.value} {...props}
                 onFocus={() => {
-                    props?.formControl?.patchTouched(true);
-                    if(defaultOnFocus){
+                    formControl?.patchTouched(true);
+                    if (defaultOnFocus) {
                         defaultOnFocus();
                     }
                 }
                 }
                 onChange={(e) => {
-                    props?.formControl?.patchValue(e.target.value);
+                    formControl?.patchValue(e.target.value);
                     if (defaultOnChange) {
                         defaultOnChange();
                     }
