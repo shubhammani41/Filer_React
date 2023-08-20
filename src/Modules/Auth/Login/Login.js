e8ab4955d5dc727057f081748767d043ea05a165
shubhammani41 <shubhammani41@gmail.com>
2023-08-20 23:54:11 +0530

    FormGroup is not possible

--- src/Modules/Auth/Login/Login.js
+++ src/Modules/Auth/Login/Login.js
@@ -6,18 +6,10 @@ import { Link } from "react-router-dom";
 import { FormControl } from '../../Global/ReactiveForms/FormControl';
 import { TextFieldReactive } from '../../Global/TextFieldReactive/TextFieldReactive';
 import { Validators } from '../../Global/ReactiveForms/Validators';
-import { FormGroup } from '../../Global/ReactiveForms/FormGroup';
 
 function Login() {
     let userName = new FormControl('', [Validators.Required]);
-    let password = new FormControl('', []);
-    let group = new FormGroup({
-        name:['',[Validators.Required]],
-    })
-    const patchGroup = () => {
-        password.patchValidators([Validators.Required]);
-        group.patchGroup({name:['m']})
-    }
+    let password = new FormControl('', [Validators.Required]);
     return (
         <div className="LoginContainer">
             <div className="LandingCardContainer">
@@ -28,8 +20,6 @@ function Login() {
                             formControl={userName} error={userName.invalid && userName.touched} />
                         <TextFieldReactive label={strObjects.passowrd} size='small' className='mb15 mt5' id="username" variant="outlined" placeholder={strObjects.place_holder_password}
                             formControl={password} error={password.invalid && password.touched} />
-                        {/* <InputLabel htmlFor="password">{strObjects.passowrd}</InputLabel>
-                        <TextField size='small' className="mt5" id="password" variant="outlined" placeholder={strObjects.place_holder_password} /> */}
                     </CardContent>
                     <CardActions>
                         <div className='W100'>
@@ -39,9 +29,6 @@ function Login() {
                             <div className='V_Center H_Center W100'>
                                 <Link to="/signup"><Button color='primary' variant='text' size="small">{strObjects.signup}</Button></Link>
                             </div>
-                            <button onClick={() => { patchGroup() }}>
-                                patch
-                            </button>
                         </div>
                     </CardActions>
                 </Card>
